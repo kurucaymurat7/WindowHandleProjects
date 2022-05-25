7d71d752e1c170efd25bd0c6fe6bf2c1a5a07332
Murat K <kurucaymurat7@gmail.com>
2022-05-25 20:01:11 +0300

    master commit1

--- src/test/java/WindowHandleSolutions/MouseActions1.java
+++ src/test/java/WindowHandleSolutions/MouseActions1.java
@@ -42,6 +42,10 @@ public class MouseActions1 {
         driver.get("https://www.walmart.com");
         //● Sayfa title’nin “Walmart” icerdigini test edin
 
+
+        System.out.println("bu da master değişiklik 1");
+        driver.navigate().to("https://www.google.com");
+
         //2- https://the-internet.herokuapp.com/context_menu sitesine gidelim
         driver.get("https://the-internet.herokuapp.com/context_menu");
         //3- Cizili alan uzerinde sag click yapalim
