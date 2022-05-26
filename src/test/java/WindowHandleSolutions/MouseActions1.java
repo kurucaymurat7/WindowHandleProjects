870d55520a8a760acb2c02bde98b55227554b992
Murat K <kurucaymurat7@gmail.com>
2022-05-26 10:50:29 +0300

    sacma commit

--- src/test/java/WindowHandleSolutions/MouseActions1.java
+++ src/test/java/WindowHandleSolutions/MouseActions1.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.interactions.Actions;
 import java.time.Duration;
 import java.util.Set;
 
-public class MouseActions1 {
+public class MouseActions1 extends WindowHandle_C02{
     //1- Yeni bir class olusturalim: MouseActions1
     //2- https://the-internet.herokuapp.com/context_menu sitesine gidelim
     //3- Cizili alan uzerinde sag click yapalim
@@ -69,14 +69,72 @@ public class MouseActions1 {
         System.out.println("ahmet ekledi");
         driver.get("https://www.google.com");
 
+        //● Amazon anasayfa adresine gidin.
+        driver.get("https:www.amazon.com");
+        //● Sayfa’nin window handle degerini String bir degiskene atayin
+        String amazonWindowHandleDegeri = driver.getWindowHandle();
+        //● Sayfa title’nin “Amazon” icerdigini test edin
+        String expectedTitle = "Amazon";
+        String actualTitle = driver.getTitle();
+
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        driver.switchTo().window(amazonWindowHandleDegeri);
+        //● Sayfa title’nin “Amazon” icerdigini test edin
+        expectedTitle = "Amazon";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Yeni bir tab olusturup, acilan tab’da techproeducation.com adresine gidin
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.techproeducation.com");
+        //● Sayfa title’nin “TECHPROEDUCATION” icerdigini test edin
+        expectedTitle = "Techpro Education";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Yeni bir window olusturup, acilan sayfada walmart.com adresine gidin
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.walmart.com");
+        //● Sayfa title’nin “Walmart” icerdigini test edin
+        expectedTitle = "Walmart";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        driver.switchTo().window(amazonWindowHandleDegeri);
+        //● Sayfa title’nin “Amazon” icerdigini test edin
+        expectedTitle = "Amazon";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
         System.out.println("bu satırı da sildi");
         driver.switchTo().window(ikinciSayfaWindowHandleDegeri);
         WebElement acilanSayfaTag = driver.findElement(By.xpath("//h1"));
 
         System.out.println("ahmet ilk satir");
 
-        String expectedTitle = "Elemental Selenium";
-        String actualTitle = acilanSayfaTag.getText();
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        driver.switchTo().window(amazonWindowHandleDegeri);
+        //● Sayfa title’nin “Amazon” icerdigini test edin
+        expectedTitle = "Amazon";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        expectedTitle = "Elemental Selenium";
+        actualTitle = acilanSayfaTag.getText();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        //● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        driver.switchTo().window(amazonWindowHandleDegeri);
+        //● Sayfa title’nin “Amazon” icerdigini test edin
+        expectedTitle = "Amazon";
+        actualTitle = driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedTitle));
 
         System.out.println("ahmet ekledi");
